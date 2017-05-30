5cb63e12826f8931410625eff49e8b457b0f3469
Leon Machens <leon.machens@googlemail.com>
2017-05-30 22:11:34 +0200

    create default admin

---
+++ kadira-ui/server/startup.js
@@ -0,0 +1,9 @@
+Meteor.startup(() =>{
+  if (!Meteor.users.findOne({ username: "admin" })) {
+    Accounts.createUser({
+      username: "admin",
+      email: "admin@admin.com",
+      password: "admin"
+    });
+  }
+})
