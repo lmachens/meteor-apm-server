e8ab78d55ce357ddc45cc20132f13eafdea5ba6a
Leon Machens <leon.machens@googlemail.com>
2017-05-31 08:34:19 +0200

    update environment

--- init-shell.sh
+++ init-shell.sh
@@ -1,8 +1,8 @@
 # DB settings
-export APP_MONGO_URL="mongodb://app:password@candidate.53.mongolayer.com:10478,candidate.54.mongolayer.com:10216/tkadira-app?replicaSet=set-56175e62147ca745d8000761"
-export APP_MONGO_OPLOG_URL="mongodb://oplog:password@candidate.53.mongolayer.com:10478,candidate.54.mongolayer.com:10216/local?authSource=tkadira-app&replicaSet=set-56175e62147ca745d8000761"
-export DATA_MONGO_URL="mongodb://app:password@candidate.53.mongolayer.com:10478,candidate.54.mongolayer.com:10216/tkadira-data?replicaSet=set-56175e62147ca745d8000761"
-export MAIL_URL="smtp://postmaster%40kadira.io:9jx4fqhdfbg5@smtp.mailgun.org:587"
+export APP_MONGO_URL="mongodb://localhost:27017/kadira"
+export APP_MONGO_OPLOG_URL="mongodb://localhost:27017/local"
+export DATA_MONGO_URL="mongodb://localhost:27017/kadira"
+#export MAIL_URL="smtp://postmaster%40kadira.io:9jx4fqhdfbg5@smtp.mailgun.org:587"
 
 # Engine settings
 export ENGINE_PORT=11011
@@ -13,5 +13,6 @@ export UI_URL="http://localhost:$UI_PORT"
 
 # Monitoring Setup
 
-export LIBRATO_EMAIL
-export LIBRATO_TOKEN
+#export LIBRATO_EMAIL
+#export LIBRATO_TOKEN
+
