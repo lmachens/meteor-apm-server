41d72d5e2f3f5439de01fae759ab3ccfb4f1a885
Leon Machens <leon.machens@googlemail.com>
2017-09-27 12:16:18 +0200

    use timeout instead of interval

--- apm/server/rma/server.js
+++ apm/server/rma/server.js
@@ -1,24 +1,24 @@
 Fiber = Npm.require('fibers');
 
-Meteor.startup(() => {
-    function runAll() {
-        Fiber(function() {
-            //console.log('start incremental aggregation');
-            incrementalAggregation(PROFILES['1min'], PROVIDERS['errors']);
-            incrementalAggregation(PROFILES['1min'], PROVIDERS['methods']);
-            incrementalAggregation(PROFILES['1min'], PROVIDERS['pubsub']);
-            incrementalAggregation(PROFILES['1min'], PROVIDERS['system']);
-            incrementalAggregation(PROFILES['30min'], PROVIDERS['errors']);
-            incrementalAggregation(PROFILES['30min'], PROVIDERS['methods']);
-            incrementalAggregation(PROFILES['30min'], PROVIDERS['pubsub']);
-            incrementalAggregation(PROFILES['30min'], PROVIDERS['system']);
-            incrementalAggregation(PROFILES['3hour'], PROVIDERS['errors']);
-            incrementalAggregation(PROFILES['3hour'], PROVIDERS['methods']);
-            incrementalAggregation(PROFILES['3hour'], PROVIDERS['pubsub']);
-            incrementalAggregation(PROFILES['3hour'], PROVIDERS['system']);
-        }).run();
-    }
+function runAll() {
+    Fiber(function() {
+        //console.log('start incremental aggregation');
+        incrementalAggregation(PROFILES['1min'], PROVIDERS['errors']);
+        incrementalAggregation(PROFILES['1min'], PROVIDERS['methods']);
+        incrementalAggregation(PROFILES['1min'], PROVIDERS['pubsub']);
+        incrementalAggregation(PROFILES['1min'], PROVIDERS['system']);
+        incrementalAggregation(PROFILES['30min'], PROVIDERS['errors']);
+        incrementalAggregation(PROFILES['30min'], PROVIDERS['methods']);
+        incrementalAggregation(PROFILES['30min'], PROVIDERS['pubsub']);
+        incrementalAggregation(PROFILES['30min'], PROVIDERS['system']);
+        incrementalAggregation(PROFILES['3hour'], PROVIDERS['errors']);
+        incrementalAggregation(PROFILES['3hour'], PROVIDERS['methods']);
+        incrementalAggregation(PROFILES['3hour'], PROVIDERS['pubsub']);
+        incrementalAggregation(PROFILES['3hour'], PROVIDERS['system']);
+        setTimeout(runAll, 60000);
+    }).run();
+}
 
+Meteor.startup(() => {
     runAll();
-    setInterval(runAll, 60000);
 });
