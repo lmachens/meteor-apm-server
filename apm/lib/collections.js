e17fad7e582305c577c6915ac147d6574c9f74d6
Leon Machens <leon.machens@googlemail.com>
2017-10-03 10:21:06 +0200

    add indexes

--- apm/lib/collections.js
+++ apm/lib/collections.js
@@ -7,6 +7,7 @@ ErrorsMeta = new Mongo.Collection("errorsMeta");
 MapReduceProfileConfig = new Mongo.Collection("mapReduceProfileConfig");
 RawErrorMetrics = new Mongo.Collection("rawErrorMetrics");
 ErrorMetrics = new Mongo.Collection("errorMetrics");
+AppStats = new Mongo.Collection("appStats");
 ProdStats = new Mongo.Collection("prodStats");
 MethodsMetrics = new Mongo.Collection("methodsMetrics");
 RawMethodsMetrics = new Mongo.Collection("rawMethodsMetrics");
@@ -15,3 +16,56 @@ RawPubMetrics = new Mongo.Collection("rawPubMetrics");
 SystemMetrics = new Mongo.Collection("systemMetrics");
 RawSystemMetrics = new Mongo.Collection("rawSystemMetrics");
 RmaLogs = new Mongo.Collection("rmaLogs");
+
+AppStats.rawCollection().createIndex({
+  "value.appId": 1,
+  "value.host": 1,
+  "value.startTime": 1
+});
+SystemMetrics.rawCollection().createIndex({
+  "value.appId": 1,
+  "value.host": 1,
+  "value.startTime": 1
+});
+MethodTraces.rawCollection().createIndex({ appId: 1, host: 1, startTime: 1 });
+PubTraces.rawCollection().createIndex({ appId: 1, host: 1, startTime: 1 });
+ErrorTraces.rawCollection().createIndex({ appId: 1, host: 1, startTime: 1 });
+RawMethodsMetrics.rawCollection().createIndex({
+  "value.appId": 1,
+  "value.host": 1,
+  "value.startTime": 1
+});
+MethodsMetrics.rawCollection().createIndex({
+  "value.appId": 1,
+  "value.host": 1,
+  "value.startTime": 1
+});
+RawPubMetrics.rawCollection().createIndex({
+  "value.appId": 1,
+  "value.host": 1,
+  "value.startTime": 1
+});
+PubMetrics.rawCollection().createIndex({
+  "value.appId": 1,
+  "value.host": 1,
+  "value.startTime": 1
+});
+RawSystemMetrics.rawCollection().createIndex({
+  "value.appId": 1,
+  "value.host": 1,
+  "value.startTime": 1
+});
+RawErrorMetrics.rawCollection().createIndex({
+  "value.appId": 1,
+  "value.host": 1,
+  "value.startTime": 1
+});
+ErrorMetrics.rawCollection().createIndex({
+  "value.appId": 1,
+  "value.host": 1,
+  "value.startTime": 1
+});
+ProdStats.rawCollection().createIndex({
+  appId: 1,
+  metric: 1
+});
