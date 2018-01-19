24379a0a0dfb710d2af60174ee695ed0fc2b05ba
EricH <AvarioDeveloper@gmail.com>
2018-01-19 14:45:17 -0500

    Fix copy paste error

--- apm/server/rma/server.js
+++ apm/server/rma/server.js
@@ -39,10 +39,10 @@ async function runMedium() {
 async function runLong() {
   const startTime = new Date();
 
-  await incrementalAggregation(PROFILES['30min'], PROVIDERS['errors']);
-  await incrementalAggregation(PROFILES['30min'], PROVIDERS['methods']);
-  await incrementalAggregation(PROFILES['30min'], PROVIDERS['pubsub']);
-  await incrementalAggregation(PROFILES['30min'], PROVIDERS['system']);
+  await incrementalAggregation(PROFILES['3hour'], PROVIDERS['errors']);
+  await incrementalAggregation(PROFILES['3hour'], PROVIDERS['methods']);
+  await incrementalAggregation(PROFILES['3hour'], PROVIDERS['pubsub']);
+  await incrementalAggregation(PROFILES['3hour'], PROVIDERS['system']);
 
   cleanup(startTime);
 
